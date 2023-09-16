85f2eeeb88a1da54eca4cc4593a70da49187966d
noah <jeonghogim318@gmail.com>
2023-09-16 15:18:50 +0900

    섹션4 - 회원 기능 테스트

--- src/main/java/jpabook/jpabook/domain/Order.java
+++ src/main/java/jpabook/jpabook/domain/Order.java
@@ -43,7 +43,7 @@ public class Order {
     }
 
     public void addOrderItem(OrderItem orderItem){
-        orderItem.add(orderItem);
+        orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
 
