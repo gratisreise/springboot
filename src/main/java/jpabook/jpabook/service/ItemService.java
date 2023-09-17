fa1cd68f1671e9140915df2ebc28c56ecd698e20
noah <jeonghogim318@gmail.com>
2023-09-17 13:47:20 +0900

    섹션5 - 주문,주문상품 엔티티 개발

--- src/main/java/jpabook/jpabook/service/ItemService.java
+++ src/main/java/jpabook/jpabook/service/ItemService.java
@@ -1,11 +1,13 @@
 package jpabook.jpabook.service;
 
-import jpabook.jpabook.domain.Item;
+import jpabook.jpabook.domain.*;
 import jpabook.jpabook.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -27,5 +29,9 @@ public class ItemService {
     public Item findOne(Long itemid){
         return itemRepository.findOne(itemid);
     }
+
+
+
+
 
 }
